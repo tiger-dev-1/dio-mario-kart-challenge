ec71f6fb448162ba3051c442a46967c52981c50c
JP <jota.tiger.dev@gmail.com>
2025-07-25 22:19:17 -0300

    create arrays: characters, trackBlocks, dice

--- index.js
+++ index.js
@@ -1,2 +1,61 @@
-console.log("Mario Kart JS is running!");
+const characters = [
+  {
+    id: 1,
+    name: "Mario",
+    speed: 4,
+    handling: 3,
+    power: 3,
+  },
+  {
+    id: 2,
+    name: "Peach",
+    speed: 3,
+    handling: 4,
+    power: 2,
+  },
+  {
+    id: 3,
+    name: "Yoshi",
+    speed: 2,
+    handling: 4,
+    power: 3,
+  },
+  {
+    id: 4,
+    name: "Bowser",
+    speed: 5,
+    handling: 2,
+    power: 5,
+  },
+  {
+    id: 5,
+    name: "Luigi",
+    speed: 3,
+    handling: 4,
+    power: 4,
+  },
+  {
+    id: 6,
+    name: "Donkey Kong",
+    speed: 2,
+    handling: 2,
+    power: 5,
+  },
+];
 
+const trackBlocks = [
+  {
+    id: 1,
+    name: "STRAIGHT",
+  },
+  {
+    id: 2,
+    name: "CURVE",
+  },
+  {
+    id: 3,
+    name: "CLASH",
+  },
+];
+
+const dice = [1, 2, 3, 4, 5, 6];
